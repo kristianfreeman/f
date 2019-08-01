90529355bb32216d8c5d04c2b496197cf6249216
Kristian Freeman <kristian@kristianfreeman.com>
2019-08-01 11:40:31 -0500

    Add structopts and handle CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate structopt;
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "f")]
+struct Opt {
+    #[structopt(long = "add")]
+    add: Option<String>,
+
+    #[structopt(name = "PATH", parse(from_os_str))]
+    path: Option<PathBuf>,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    match (opt.add, opt.path) {
+        (None, None) => println!("Nothing"),
+        (Some(add), Some(path)) => println!("{:#?} {:#?}", add, path),
+        (Some(add), None) => println!("{:#?}", add),
+        (None, Some(path)) => println!("{:#?}", path),
+    }
 }
